db7e6a840cbdb46968a44e8beca294d4ba935614
sachins0412 <saxenas964@gmail.com>
2022-08-08 22:58:58 +0530

    set up basic events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
-io();
+const socket = io();
+
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+document.querySelector("#chatMessage").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message);
+});
--- src/index.js
+++ src/index.js
@@ -11,9 +11,18 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 const io = socketio(server);
-
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("new connection");
+
+  socket.emit("message", "Welcome");
+  socket.broadcast.emit("message", "a new user has joined");
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "a user has left the chat");
+  });
 });
 
 const publicPath = path.join(__dirname, "../public");
