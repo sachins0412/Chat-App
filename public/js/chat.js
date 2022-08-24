727c6c598facd27cac5587d86bff176e6c512614
sachins0412 <saxenas964@gmail.com>
2022-08-24 21:25:19 +0530

    handle error from server

--- public/js/chat.js
+++ public/js/chat.js
@@ -67,4 +67,9 @@ socket.on("locationMessage", (location) => {
   $message.insertAdjacentHTML("beforeend", html);
 });
 
-socket.emit("join", { username, room }, (error) => {});
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
+});
